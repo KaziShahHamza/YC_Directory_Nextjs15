d4ff148268eb2bff3d8a493f4ae85035b543bb9b
soulfulDreamer <kazishahhamza@outlook.com>
2024-12-30 01:28:29 +0600

    startup box added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -6,6 +7,20 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 100,
+      author: { _id: 1, name: 'Jenny' },
+      _id: 1,
+      description:
+        "A platform to connect with other entrepreneurs and pitch their startups to the community.",
+      image: "https://picsum.photos/400/300",
+      category: "Tech",
+      title: "We Robots",
+    },
+  ];
 
   return (
     <>
@@ -18,6 +33,22 @@ export default async function Home({
           pitch their startups to the community.
         </p>
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startups Found</p>
+          )}
+        </ul>
       </section>
     </>
   );
