c4836a140d89d5a1aa79457bf0ddba2293a43a2b
soulfulDreamer <kazishahhamza@outlook.com>
2025-01-03 19:09:26 +0600

    sanity schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import "easymde/dist/easymde.min.css";
 
 const workSans = localFont({
   src: [
@@ -50,7 +51,7 @@ const workSans = localFont({
       style: "normal",
     },
   ],
-  variable: '--font-work-sans',
+  variable: "--font-work-sans",
 });
 
 export const metadata: Metadata = {
@@ -65,11 +66,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={workSans.variable} 
-      >
-        {children}
-      </body>
+      <body className={workSans.variable}>{children}</body>
     </html>
   );
 }
