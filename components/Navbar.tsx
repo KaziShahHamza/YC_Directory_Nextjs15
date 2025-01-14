0cd1c33ad9f2783122e933a39e11d49e532d0178
soulfulDreamer <kazishahhamza@outlook.com>
2025-01-14 15:58:29 +0600

    startup form design

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = async () => {
           {session && session?.user ? (
             <>
               <Link href="/startup/create">
-                <span>Created</span>
+                <span>Create</span>
               </Link>
 
               <form
